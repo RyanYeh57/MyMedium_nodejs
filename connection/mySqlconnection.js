246875357313f1358c08ec03d997d31af59023ad
Ryan Yeh <ej68433221ik@gmail.com>
2022-10-05 14:07:43 +0800

    set mySql connection

---
+++ connection/mySqlconnection.js
@@ -0,0 +1,17 @@
+const mySQL = require('mysql');
+const config = require('config');
+
+const connection = mySQL.createConnection(
+  {
+    host: config.get("host"),
+    user: config.get("user"),
+    password: config.get("password"),
+    database: config.get("database")
+  }
+)
+
+connection.connect((err) => {
+  if (err) throw err;
+});
+
+module.exports = connection;
